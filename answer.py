dc03efce263b0fe673fd9c8f6bf2999005b03079
hgu <hguhelen@gmail.com>
2019-04-25 16:40:23 -0400

    good version to submit

--- answer.py
+++ answer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import sys
 import math
 from collections import Counter
@@ -6,45 +7,96 @@ import spacy
 import warnings
 warnings.filterwarnings("ignore")
 
-def bestPara(article, questions):
+"""def allChildren(t):
+    # input should be a token
+    # return all the children as a string without considering the actual order
+    # or content of the sentence
+    #############################
+    ### I personally like allChildrenIndex better
+    #############################
+
+    l = [w for w in t.children]
+    if len(l) == 0:
+        return t.text
+    else:
+        return " ".join([allChildren(c) for c in t.children])"""
+
+def allChildrenIndexHelper(t):
+    l = [w for w in t.children]
+    if len(l) == 0:
+        #print(l, str(t.i))
+        return [t.i]
+    else:
+        #print(l, [allChildrenIndexHelper(c) for c in t.children])
+        return [elem for elems in [allChildrenIndexHelper(c) for c in t.children]for elem in elems] + [t.i]
+
+def allChildrenIndex(t):
+    # input should be a token
+    # Returning the index for the child clause
+    # Output: start, end
+    # child clause would be st[start, end] where st is the original sentence
+    allIndexes = [elem for elem in allChildrenIndexHelper(t)]
+    return min(allIndexes), max(allIndexes)+1
+
+"""def bestPara(article, question, kword):
     # sim: a 2D array
     # sim[i][j]: cos sim for question i and paragraph j
-    sim = [[0]*len(article)]*len(questions)
     nlp = spacy.load("en_core_web_sm")
+    article = article.split("\n")
+    bstP = [0] * len(article)
+    qNLP = nlp(question)
 
-    # compute all values in sim
-    for j in range(len(article)):
-        # note that para is a string of several sentences
-        parNLP = nlp(article[j])
-        for i in range(len(questions)):
-            sim[i][j] = parNLP.similarity(nlp(questions[i]))
+    bstP = [qNLP.similarity(nlp(art)) for art in article]
+    #print(bstP)
 
-    # find the best matching paragraph for each question
-    # find the max index for each sim[i] (question i)
+    inds = [ind for ind, v in sorted(enumerate(bstP), key = lambda x: x[1], reverse=True)[:10]]
+    for p in [article[ind] for ind in inds]:
+        if kword in p:
+            return p
+    return [article[ind] for ind in inds][0]"""
 
-    # bstParagraph[i]: the index of best matching para for question i
-    # iow, article[bstParagraph[i]]: best matching paragraph for question i
-    bstParagraph = [0] * len(questions)
-    for i in range(len(sim)):
-        ind, v = max(enumerate(sim[i]), key = lambda x: x[1])
-        bstParagraph[i] = ind
-    return bstParagraph
-
-def bestSent(parag, ques):
+def bestSent(parag, ques, kword):
     # parag and ques are string
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(parag)
     sents = list(doc.sents)
-    bstS = [0]*len(sents)
 
     qNLP = nlp(ques)
     bstS = [qNLP.similarity(nlp(s.text)) for s in sents]
 
-    ind, v = max(enumerate(bstS), key = lambda x: x[1])
-    return sents[ind].text
+
+    inds = [ind for ind, v in sorted(enumerate(bstS), key = lambda x: x[1], reverse=True)[:10]]
+    for s in [sents[ind] for ind in inds]:
+        if kword in s.text:
+            return s
+    return [sents[ind] for ind in inds][0]
+
+    #ind, v = max(enumerate(bstS), key = lambda x: x[1])
+    #return sents[ind].text
 
 def whoAnswer(st, qt):
-    # When modifying, consider both NER and dep
+    r = ""
+    for i in range(len(qt)):
+        if qt[i].dep_ == "ROOT":
+            r = qt[i]
+            break
+
+    s = st.text.split()
+    if r.text in st.text and r.text in s:
+        for i in range(s.index(r.text), len(st)):
+            if st[i].text == r.text:
+                #print(list(st.noun_chunks))
+                children = [allChildrenIndex(c) for c in list(st[i].children) 
+                if "nsubj" in c.dep_]
+                children = [st[a:b] for a, b in children]
+
+                if len(children)>0:
+                    nlp = spacy.load("en_core_web_sm")
+                    ind, v = max(enumerate([sub.similarity(qt) for sub in 
+                        children]), key = lambda x: x[1])
+                    return children[ind]
+                    
+    # Otherwise
     ans = ""
     for i in range(len(st)):
         if st[i].dep_=="ROOT":
@@ -61,10 +113,30 @@ def whoAnswer(st, qt):
                 if lfts[l].dep_ == "nmod":
                     ans = lfts[l].text + " " + ans
             return ans
-    return "None Found"
+    return st.text
 
 def whatAnswer(st, qt):
-    # When modifying, consider both NER and dep
+    r = ""
+    for i in range(len(qt)):
+        if qt[i].dep_ == "ROOT":
+            r = qt[i]
+            break
+
+    s = st.text.split()
+    if r.text in st.text and r.text in s:
+        for i in range(s.index(r.text), len(st)):
+            if st[i].text == r.text:
+                children = [allChildrenIndex(c) for c in list(st[i].children) 
+                if "nsubj" in c.dep_]
+                children = [st[a:b] for a, b in children]
+
+                if len(children)>0:
+                    nlp = spacy.load("en_core_web_sm")
+                    ind, v = max(enumerate([sub.similarity(qt) for sub in 
+                        children]), key = lambda x: x[1])
+                    return children[ind]
+
+    # Otherwise
     ans = ""
     for i in range(len(st)):
         if st[i].dep_=="ROOT":
@@ -77,24 +149,50 @@ def whatAnswer(st, qt):
                 if lfts[l].dep_ == "nmod":
                     ans = lfts[l].text + " " + ans
             return ans
-    return "None Found"
+    return st.text
 
 def whenAnswer(st, qt):
     ans = ""
     #print([w.dep_ for w in st])
-    return "None Found"
+    return st.text
 
 def whereAnswer(st, qt):
-    return "None Found"
+    return st.text
 
 def whyAnswer(st, qt):
-    return "None Found"
+    because = -1
+    subj = 0
+    root = ""
+    for token in qt:
+        if token.dep_ == "ROOT":
+            root = token
+            # Find the subject of the sentence
+            for child in token.children:
+                if (child.dep_ == "nsubj" or child.dep_ == "nsubjpass"):
+                    subj = child.i
+                    break
+    for token in qt:
+        if token.text.lower() == "because": because = token.i
+
+    if (because == -1): return st.text
+
+    if (subj > because):
+        sentence = d[because:subject].text
+        sentence.rstrip(",")
+        return sentence
+    else:
+        #first_nounphrase = list(qt.noun_chunks)[0]
+        sentence = st[because:].text
+        return sentence
 
 def howAnswer(st, qt):
-    return "None Found"
+    return st.text
 
 def YNAnswer(st, qt):
-    return "None Found"
+    if [t.dep_ for t in st].count('neg') % 2:
+        # odd case, negation
+        return "No"
+    return "Yes"
 
 def generateAns(s, q):
     # Who, where, when, what, which, why, how, yes/no
@@ -119,8 +217,57 @@ def generateAns(s, q):
         ans = YNAnswer(st, qt)
     return ans
 
-def main():
+def simp(s):
+    nlp = spacy.load("en_core_web_sm")
+    st = nlp(s)
+    sts = []
+    punctuations = []
+    temp = 0
+    roott = ""
+    for i in range(len(st)):
+        if st[i].dep_ == "ROOT":
+            roott = st[i]
+        if st[i].dep_ == 'punct':
+            sts += [(temp, i)]
+            punctuations += [i]
+            temp = i+1
 
+    sentReturn = []
+
+    if len(sts) == len(punctuations):
+        noooo = ['because']
+        a, b = sts[0]
+        short = st[a:b]
+        flag = False
+        for w in short:
+            if w in roott.children:
+                flag = True
+                break
+        if (flag or roott in short) and st[a].text.lower() != 'because':
+            sentReturn += [[a, b]]
+
+        for i in range(len(sts)-1):
+            a, b = sts[i+1]
+            if st[punctuations[i]].text not in [';', ','] and flag:
+                sentReturn += [[punctuations[i], b]]
+            else:
+                short = st[a:b]
+                flag = False
+                for w in short:
+                    if w in roott.children:
+                        flag = True
+                        break
+                if (flag or roott in short) and st[a].text.lower() != 'because':
+                    sentReturn += [[punctuations[i], b]]
+    else:
+        return s
+    ans = ""
+    for (a, b) in sentReturn:
+        ans += st[a:b].text
+    return ans + '.'
+
+
+def main():
     pathArticle, pathQuestions = sys.argv[1:3] # article and questions path
 
     ###############
@@ -154,9 +301,17 @@ def main():
     #for i in range(len(bestMatchingP)):
     #    bestMatchingS[i] = bestSent(text[bestMatchingP[i]], qs[i])
 
+    bestMatchingP = [0] * len(qs)
     bestMatchingS = [0] * len(qs)
     for i in range(len(qs)):
-        bestMatchingS[i] = bestSent(text, qs[i])
+        nlp = spacy.load("en_core_web_sm")
+        qtn = nlp(qs[i])
+        for j in range(len(qtn)):
+            if qtn[j].dep_ == "ROOT":
+                kword = qtn[j].text
+        #bestMatchingP[i] = bestPara(text, qs[i], kword)
+        bestMatchingS[i] = bestSent(text, qs[i], kword)
+
 
     ##################################################
     ### Finding Answer from Best Matching Sentence ###
@@ -165,7 +320,7 @@ def main():
     # find the answer
     ans = []
     for i in range(len(bestMatchingS)):
-        ans.append(generateAns(bestMatchingS[i], qs[i]))
+        ans.append(generateAns(bestMatchingS[i].text, qs[i]))
 
     for a in ans:
         print(a)
